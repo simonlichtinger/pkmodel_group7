d6ca07f8a41b10ae8cc951cbbc093e077acee8b9
SalahKouhen <73072331+SalahKouhen@users.noreply.github.com>
2020-10-22 12:08:56 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../pkmodel'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
