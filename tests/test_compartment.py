3ed6af4ea9fa67284e602cda7878a03ab04853df
Simon Lichtinger <56595507+simonlichtinger@users.noreply.github.com>
2020-10-22 10:31:51 +0100

    Update test_compartment.py

--- tests/test_compartment.py
+++ tests/test_compartment.py
@@ -4,18 +4,20 @@ import pytest
 
 def test_simple_differential_eq():
     from pkmodel.compartment import Compartment
-    test_compartment = Compartment(0, 1, lambda t,q: 1, lambda t,q: q[0])
+    test_compartment = Compartment(0, 1, lambda t, q: 1, lambda t, q: q[0])
 
-    assert test_compartment.differential_eq(0,[0]) == 1
-    assert test_compartment.differential_eq(0,[2]) == -1
+    assert test_compartment.differential_eq(0, [0]) == 1
+    assert test_compartment.differential_eq(0, [2]) == -1
 
 def test_invalid_function_input():
     from pkmodel.compartment import Compartment
 
-    test_compartment = Compartment(0,1, lambda t: 1,lambda t,q: q[0]) # Input has wrong number of arguments
+    test_compartment = Compartment(
+        0, 1, lambda t: 1, lambda t, q: q[0]
+    )  # Input has wrong number of arguments
     with pytest.raises(TypeError):
-        test_compartment.differential_eq(0,[0])
+        test_compartment.differential_eq(0, [0])
 
-    test_compartment = Compartment(0,1, "Not a function",lambda t,q: q[0])
+    test_compartment = Compartment(0, 1, "Not a function", lambda t, q: q[0])
     with pytest.raises(TypeError):
-        test_compartment.differential_eq(0,[0])
+        test_compartment.differential_eq(0, [0])
