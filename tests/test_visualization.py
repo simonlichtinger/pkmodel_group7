776032d9944c113c8a556b440666ba23bb8543ff
Andrew Kirby <andrew.kirby@trinity.ox.ac.uk>
2020-10-23 16:19:40 +0100

    #23 Add test for visualisation

---
+++ tests/test_visualization.py
@@ -0,0 +1,10 @@
+def test_plot():
+    """Test that the pkanalysis file run"""
+    from pkmodel.pkanalysis import plot_solution
+    from pkmodel.pk_model import PKModel
+    import numpy as np
+    test_model = PKModel()
+    test_model.create_model("central", 1)
+    model_out = test_model.solve(np.linspace(0, 1, 1000), np.array([0.0]))
+    plot_solution(model_out, test_model.get_compartment_names)
+    assert True
